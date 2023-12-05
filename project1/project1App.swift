e9c956078b6e82a3e63222e4e3105d3601b7d0bf
velgor <esquivelguillerm@gmail.com>
2023-12-05 16:51:25 -0600

    Initial Commit

---
+++ project1/project1App.swift
@@ -0,0 +1,17 @@
+//
+//  project1App.swift
+//  project1
+//
+//  Created by Gdaalumno on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct project1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
